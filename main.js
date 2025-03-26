7a5942dd997c78a1d8aff85baf038eeb558a5faa
heyitsdurdona <durdona.developer@gmail.com>
2025-03-26 13:26:36 +0500

    add: ball rotation

--- main.js
+++ main.js
@@ -10,10 +10,26 @@ const randomY = Math.random() * (viewportHeight - elBall.offsetHeight);
 elBall.style.left = `${randomX}px`;
 elBall.style.top = `${randomY}px`;
 
+
+let rotation = 0;
+
 elBody.addEventListener('click', function(evt) {
     elSound.play();
+    elBall.classList.add('rotating');
     const {x, y} = evt;
-    elBall.style.left = `${x - elBall.width / 2}px`;
-    elBall.style.top = `${y - elBall.height / 2}px`;    
-
+    elBall.style.left = `${x -40}px`;
+    elBall.style.top = `${y - 40}px`;   
+    
+    elBall.style.transform = `rotate(${getRandomNumber(rotation)}deg)`
 });
+
+function getRandomNumber(old) { 
+    if (old == 360){
+        rotation =0;
+        return rotation;
+    }
+    else{
+        rotation = 360;
+        return rotation;
+    }
+}
